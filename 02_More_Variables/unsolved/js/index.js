55008917c212bf4cdfecb62be9f3c3a7e12a0e21
lindagogo <64371553+lindagogo@users.noreply.github.com>
2021-05-15 14:05:54 -0400

    Create index.js

---
+++ 02_More_Variables/unsolved/js/index.js
@@ -0,0 +1,25 @@
+// Create the addNumbers function here.
+function addNumbers (){
+
+var	two = 2;
+var three = 3;
+var five = two + three;
+console.log(five); // this tells us that we want 
+//to see what our result is
+
+}
+// we're using this to nest a variable inside a function
+addNumbers(); // this is calling our fuction if we do not
+// call a fuction we do not get a result
+
+
+// Create the stringConcat function here.
+function stringConcat() {
+	var taco = "taco";
+	var cat = "cat";
+	var tacoCat = taco + cat ;
+
+	alert(tacoCat); // we can nest fuctions in eachother
+}
+
+stringConcat(); // this statement says lets do this.
